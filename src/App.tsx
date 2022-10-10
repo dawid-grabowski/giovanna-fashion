1ff74a4e661f81db6d8f7073224460c2d04a4463
Dawid Grabowski <kontakt@grabowskidawid.com>
2022-10-10 19:28:02 +0200

    add: inital code setup / refactor

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = (): React.ReactElement => {
+  return (
+    <div className="App">
+      Hello, giovanna!
+    </div>
+  );
+}
+
+export default App;
