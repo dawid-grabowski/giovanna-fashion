96a72dacc0f89d27259cc3df0c6acf00a40b50c6
Dawid Grabowski <kontakt@grabowskidawid.com>
2022-10-17 22:42:33 +0200

    deleted idea files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const App: React.FC = (): React.ReactElement => {
-  return (
-    <div className="App">
-      Hello, giovanna!
-    </div>
-  );
-}
+	return <div className='App'>Hello Giovanna!</div>;
+};
 
 export default App;
